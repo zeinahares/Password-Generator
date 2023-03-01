f03c48a8b4f6c61ba8a6c9dbd93b5a9f21860647
zeinahares <zeinahares@gmail.com>
2023-03-01 16:24:44 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Password Generator
+
+## Description
+
+I built this website to help anyone generate a password between 8-128 characters, with any type of characters they wish (uppercase, lowercase, numeric, and special characters).
+
+This project helped me learn a lot about functionsa and arrays in JS!
+
+## Installation
+
+N/A
+
+## Usage
+
+Click on the Generate button to begin selecting your criteria through prompts on your web browser.
+
+Then, you can input how long you want your password to be between 8-128 characters. It also asks whether you want lowercase, uppercase, numeric or special characters through a series of confirm prompts. If the length is not between 8-128 or no character type was chosen, you get an error message telling you to try again.
+
+
+![Screenshot of website blank.](./assets/images/plain.png)
+![Screenshot of website blank.](./assets/images/withprompt.png)
+![Screenshot of website with generated password.](./assets/images/withpass.png)  
+
+## License
+
+MIT License
+
+Copyright (c) 2023 zeinahares
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// event listener to start prompts and then generate password
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+
+  var newpass = [];
+  var posspass = [];
+  var lowerchars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperchars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+  var specialchars = ["~"," ","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}",";","'",":",".","/","<",">","?"];
+
+  var generate = confirm("Would you like to generate a password?");
+
+  if (generate) {
+    // 1. prompt for length - a least 8 chars and <= 128
+    var inputlength = prompt("Please input your password length between 8 and 128","8");
+
+    // if lengths not between 8-128, give error message and repeat
+
+    while (inputlength < 8 || inputlength > 128) {
+      alert("Error, you put a length not between 8 and 128. Please try again.");
+      inputlength = prompt("Please input your password length between 8 and 128","8");
+    }
+
+    inputlength = parseInt(inputlength);
+
+    // 2. asked for char types - lower, uppercase, numeric, and/or special
+    var lowerCase = confirm("Would you like lowercase characters?");
+    var upperCase = confirm("Would you like uppercase characters?");
+    var numeric = confirm("Would you like numeric characters?");
+    var special = confirm("Would you like special characters?");
+
+    // if none are selected, give error message and repeat until something is selected
+    while (lowerCase === false && upperCase === false && numeric === false && special === false) {
+      alert("Error, you didn't select any character types. At least 1 character must be selected");
+      lowerCase = confirm("Would you like lowercase characters?");
+      upperCase = confirm("Would you like uppercase characters?");
+      numeric = confirm("Would you like numeric characters?");
+      special = confirm("Would you like special characters?");
+    }
+  }
+
+  // input characters array into new pass array
+  if (lowerCase) {
+    posspass = posspass.concat(lowerchars);
+  }
+  if (upperCase) {
+    posspass = posspass.concat(upperchars);
+  }
+  if (numeric) {
+    posspass = posspass.concat(numbers);
+  }
+  if  (special) {
+    posspass = posspass.concat(specialchars);
+  }
+  
+  console.log(posspass);
+
+  for (i = 0; i < inputlength; i++) {
+    indexrandom = Math.floor(Math.random()*posspass.length);
+    newpass[i] = posspass[indexrandom];
+  }
+
+  return newpass.join("");
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
